c6913e06fc0edf16a101a016c3161281800b3115
Petr Bouianov <petr.bouianov@gmail.com>
2014-04-18 19:26:41 -0400

    Fixes issue 75
    As the array flags were already reflected in the README documentation
    and example, this only required a test

--- tests/spec/filer.spec.js
+++ tests/spec/filer.spec.js
@@ -8,6 +8,24 @@ define(["Filer"], function(Filer) {
     it("has FileSystem constructor", function() {
       expect(typeof Filer.FileSystem).to.equal('function');
     });
-  });
 
+    it("must honor the \'FORMAT\' flag", function(done) {
+      var fs = new Filer.FileSystem({name: 'local'});
+	  var fs2 = new Filer.FileSystem({name: 'local'});
+	  fs.mkdir('/test', function(err){
+	    fs2.readdir('/', function(err, list){
+	      expect(list).to.exist;
+	      expect(list).to.have.length(1);
+		  fs2 = new Filer.FileSystem({name: 'local', flags:['FORMAT']});
+		  fs2.readdir('/', function(err, list2){
+		    expect(err).to.not.exist;
+		    expect(list2).to.exist;
+		    expect(list2).to.have.length(0);
+		    done();
+		  });
+	    });
+	  });
+    });
+
+  });
 });
