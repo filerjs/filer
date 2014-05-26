2d245a0b2620fb25a60830292cb109b80f859f27
Kieran Sedgwick <kieran.sedgwick@gmail.com>
2014-05-26 16:28:35 -0400

    Removed unnecessary files

--- tests/node-runner.js
+++
@@ -1,5 +0,0 @@
-// We use Chai's expect assertions in all the tests via a global
-GLOBAL.expect = require('chai').expect;
-
-// Tests to be run are defined in test-manifest.js
-require('./test-manifest.js');
