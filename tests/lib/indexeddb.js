98cd3e6da7ca7bf0902b0dd16f31f1c1ad0106f1
David Humphrey (:humph) david.humphrey@senecacollege.ca <david.humphrey@senecacollege.ca>
2014-06-23 13:09:02 -0400

    Fix #228: Uncaught [Filer Error] Your browser doesn't support IndexedDB or WebSQL.

--- tests/lib/indexeddb.js
+++ tests/lib/indexeddb.js
@@ -1,56 +1,53 @@
-(function(global) {
-  var Filer = require("../..");
+var Filer = require("../..");
 
-  var indexedDB = global.indexedDB       ||
-                  global.mozIndexedDB    ||
-                  global.webkitIndexedDB ||
-                  global.msIndexedDB;
+var indexedDB = global.indexedDB       ||
+                global.mozIndexedDB    ||
+                global.webkitIndexedDB ||
+                global.msIndexedDB;
 
-  var needsCleanup = [];
-  if(global.addEventListener) {
-    global.addEventListener('beforeunload', function() {
-      needsCleanup.forEach(function(f) { f(); });
-    });
-  }
+var needsCleanup = [];
+if(global.addEventListener) {
+  global.addEventListener('beforeunload', function() {
+    needsCleanup.forEach(function(f) { f(); });
+  });
+}
 
-  function IndexedDBTestProvider(name) {
-    var _done = false;
-    var that = this;
+function IndexedDBTestProvider(name) {
+  var _done = false;
+  var that = this;
 
-    function cleanup(callback) {
-      if(!that.provider || _done) {
-        return;
-      }
-
-      // We have to force any other connections to close
-      // before we can delete a db.
-      if(that.provider.db) {
-        that.provider.db.close();
-      }
-
-      callback = callback || function(){};
-      var request = indexedDB.deleteDatabase(name);
-      function finished() {
-        that.provider = null;
-        _done = true;
-        callback();
-      }
-      request.onsuccess = finished;
-      request.onerror = finished;
+  function cleanup(callback) {
+    if(!that.provider || _done) {
+      return;
     }
 
-    function init() {
-      if(that.provider) {
-        return;
-      }
-      that.provider = new Filer.FileSystem.providers.IndexedDB(name);
-      needsCleanup.push(cleanup);
+    // We have to force any other connections to close
+    // before we can delete a db.
+    if(that.provider.db) {
+      that.provider.db.close();
     }
 
-    this.init = init;
-    this.cleanup = cleanup;
+    callback = callback || function(){};
+    var request = indexedDB.deleteDatabase(name);
+    function finished() {
+      that.provider = null;
+      _done = true;
+      callback();
+    }
+    request.onsuccess = finished;
+    request.onerror = finished;
   }
 
-  module.exports = IndexedDBTestProvider;
+  function init() {
+    if(that.provider) {
+      return;
+    }
+    that.provider = new Filer.FileSystem.providers.IndexedDB(name);
+    needsCleanup.push(cleanup);
+  }
 
-}(this));
+  this.init = init;
+  this.cleanup = cleanup;
+}
+
+module.exports = IndexedDBTestProvider;
