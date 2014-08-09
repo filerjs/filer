77e0ee7117064c81dfe54d3eb46c0bec38d74073
secretrobotron <secretrobotron@gmail.com>
2014-08-09 13:05:15 -0400

    [issue249] fixed Buffer prototype & tests

--- src/buffer.js
+++ src/buffer.js
@@ -9,6 +9,11 @@ function FilerBuffer (subject, encoding, nonZero) {
   return new Buffer(subject, encoding, nonZero);
 };
 
+// Inherit prototype from Buffer
+FilerBuffer.prototype = Object.create(Buffer.prototype);
+FilerBuffer.prototype.constructor = FilerBuffer;
+
+// Also copy static methods onto FilerBuffer ctor
 Object.keys(Buffer).forEach(function (p) {
   if (Buffer.hasOwnProperty(p)) {
     FilerBuffer[p] = Buffer[p];
