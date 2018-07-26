ee8e2a665a828f7ce37c362b0396f8163240d044
David Humphrey <david.humphrey@senecacollege.ca>
2018-07-26 18:01:07 -0400

    Fix lint issues in fs.writeFile-readFile.spec.js

--- tests/spec/fs.writeFile-readFile.spec.js
+++ tests/spec/fs.writeFile-readFile.spec.js
@@ -13,7 +13,6 @@ describe('fs.writeFile, fs.readFile', function() {
 
   it('should error when path is wrong to readFile', function(done) {
     var fs = util.fs();
-    var contents = 'This is a file.';
 
     fs.readFile('/no-such-file', 'utf8', function(error, data) {
       expect(error).to.exist;
@@ -67,8 +66,7 @@ describe('fs.writeFile, fs.readFile', function() {
 
   it('should write, read a binary file', function(done) {
     var fs = util.fs();
-    // String and utf8 binary encoded versions of the same thing:
-    var contents = 'This is a file.';
+    // String and utf8 binary encoded versions of the same thing: 'This is a file.'
     var binary = new Buffer([84, 104, 105, 115, 32, 105, 115, 32, 97, 32, 102, 105, 108, 101, 46]);
 
     fs.writeFile('/myfile', binary, function(error) {
