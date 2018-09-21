839ba0d73a3c380713e480c3a2552083dab588eb
U-NOTE3\y2s19 <y2s1982@gmail.com>
2018-09-20 22:15:37 -0400

    implemented first two tests of fs.promises.read

--- tests/spec/fs.read.spec.js
+++ tests/spec/fs.read.spec.js
@@ -88,3 +88,92 @@ describe('fs.read', function() {
     });
   });
 });
+
+describe('fs.promises.read', function() {
+	beforeEach(util.setup);
+	afterEach(util.cleanup);
+
+	it('should be a function', function() {
+		var fsPromises = util.fs().promises;
+		expect(fsPromises.read).to.be.a('function');
+	});
+
+	it('should read data from a file', function(done) {
+		var fsPromises = util.fs().promises;
+		var wbuffer = new Filer.Buffer([1, 2, 3, 4, 5, 6, 7, 8]);
+		var rbuffer = new Filer.Buffer(wbuffer.length);
+		var fdesc;
+		rbuffer.fill(0);
+
+		fsPromises.open('/myfile', 'w+')
+			.catch((error)=>{throw error;})
+			.then((fd)=>{fdesc=fd;return fsPromises.write(fd, wbuffer, 0, wbuffer.length, 0);})
+			.catch((error)=>{throw error;})
+			.then((result)=>{
+				expect(result).to.equal(wbuffer.length);
+				return fsPromises.read(fdesc, rbuffer,0,rbuffer.length,0);
+			})
+			.catch((error)=>{expect(error).not.to.exist;})
+			.then((result)=>{
+				expect(result).to.equal(rbuffer.length);
+				expect(wbuffer).to.deep.equal(rbuffer);
+				done();
+			})
+		;
+	});
+
+	it('should update the current file position', function(done) {
+		var fsPromises = util.fs().promises;
+		var wbuffer = new Filer.Buffer([1, 2, 3, 4, 5, 6, 7, 8]);
+		var rbuffer = new Filer.Buffer(wbuffer.length);
+		rbuffer.fill(0);
+		var _result = 0;
+
+		fsPromises.open('/myfile', 'w+', function(error, fd) {
+			if(error) throw error;
+
+			fsPromises.write(fd, wbuffer, 0, wbuffer.length, 0, function(error, result) {
+				if(error) throw error;
+				expect(result).to.equal(wbuffer.length);
+
+				fsPromises.read(fd, rbuffer, 0, rbuffer.length / 2, undefined, function(error, result) {
+					if(error) throw error;
+
+					_result += result;
+					fsPromises.read(fd, rbuffer, rbuffer.length / 2, rbuffer.length, undefined, function(error, result) {
+						if(error) throw error;
+						_result += result;
+						expect(error).not.to.exist;
+						expect(_result).to.equal(rbuffer.length);
+						expect(wbuffer).to.deep.equal(rbuffer);
+						done();
+					});
+				});
+			});
+		});
+	});
+
+	it('should fail to read a directory', function(done) {
+		var fsPromises = util.fs().promises;
+		var buf = new Filer.Buffer(20);
+		var buf2 = new Filer.Buffer(20);
+		buf.fill(0);
+		buf2.fill(0);
+
+		fsPromises.mkdir('/mydir', function(error) {
+			if(error) throw error;
+
+			fsPromises.open('/mydir', 'r', function(error, fd) {
+				if(error) throw error;
+
+				fsPromises.read(fd, buf, 0, buf.length, 0, function(error, result) {
+					expect(error).to.exist;
+					expect(error.code).to.equal('EISDIR');
+					expect(result).to.equal(0);
+					expect(buf).to.deep.equal(buf2);
+					done();
+				});
+			});
+		});
+	});
+});
