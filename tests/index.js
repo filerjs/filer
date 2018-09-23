ef80713c3317bd0cf335cd736cf2a91534363650
mordax <maya@mordax.io>
2018-09-23 19:20:16 -0400

    Added tests for future fs.copyFile implementation and adjusted index.js to run it.

--- tests/index.js
+++ tests/index.js
@@ -46,6 +46,7 @@ require('./spec/errors.spec');
 require('./spec/fs.shell.spec');
 require('./spec/fs.chmod.spec');
 require('./spec/fs.chown.spec');
+require('./spec/fs.copyFile.spec');
 
 // Filer.FileSystem.providers.*
 require('./spec/providers/providers.spec');
