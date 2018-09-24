546b4567d279f90db5bce18de8d27833927891dc
rhayes2 <hayes.ryan83@gmail.com>
2018-09-24 14:16:16 -0400

    Added Test for CHMODE

--- tests/spec/fs.chmod.spec.js
+++ tests/spec/fs.chmod.spec.js
@@ -39,6 +39,20 @@ describe('fs.chmod, fs.fchmod', function() {
     });
   });
 
+  it('Should be a valid mode value to set permissions', function(done){
+    var fs = util.fs();
+    fs.mkdir('/file',function(err, ed) {
+      if(err) throw err;
+      fs.chmod(ed, 1000, function(err) {
+        expect(err).to.exist;
+        expect(err.code).to.equal('EINVAL');
+        done();
+      });
+    });
+  });
+
+
+
   it('should allow for updating mode of a given file', function(done) {
     var fs = util.fs();
 
