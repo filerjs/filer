4a5d8c1222cb454040b28397896bbf667ba6b2fe
Dan Lin <dinahlin007@gmail.com>
2018-09-24 16:46:47 -0400

    Fix #456 - add test for fs.write() with undefined path

--- tests/spec/fs.write.spec.js
+++ tests/spec/fs.write.spec.js
@@ -4,6 +4,18 @@ var expect = require('chai').expect;
 describe('fs.write', function() {
   beforeEach(util.setup);
   afterEach(util.cleanup);
+  
+  // if the file is undefined, it will create problems
+  // 
+  it('should be a error',function(done){
+    var fs=util.fs();
+    fs.writeFile(undefined, 'data', function(error) {
+      expect(error).to.exist;
+      expect(error.code).to.equal('EINVAL');
+      done();
+    });
+  });
+
 
   it('should be a function', function() {
     var fs = util.fs();
