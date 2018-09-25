7c9d51704e2c71075839459b471cb6f5ad7dfdbd
Xiaochen Wang <ryan.wang23@hotmail.com>
2018-09-25 03:26:37 -0400

    fix issus477 should return an error if the destination path not exists

--- tests/spec/fs.symlink.spec.js
+++ tests/spec/fs.symlink.spec.js
@@ -58,57 +58,19 @@ describe('fs.promises.symlink', function(){
   });
   afterEach(util.cleanup);
 
-  // it('should be return an error if the file destination path is bot exists', function(done){
-  //   var fsPromises = util.fs().promises;
-
-  //   fsPromises.symlink('/', '/tmp/mydir', function(error){
-  //     expect(error).to.exist;
-  //     expect(error.code).to.equal('ENOENT');
-  //     done();
-  //   });
-  // });
 
   it('should be a function', function() {
     var fsPromises = util.fs().promises;
     expect(fsPromises.symlink).to.be.a('function');
   });
-  
-  it('should be a function', function() {
-    var fsPromises = util.fs().promises;
-    expect(fsPromises.symlink).to.be.a('function');
+
+
+  it('should return an error if the destination path already exists', () => {
+    var fsPromise = util.fs().promises;
+
+    return fsPromise.symlink('/', '/tmp/')
+      .then((error) =>{
+        expect(error).not.be.exist;
+      });
   });
-
-  // it('should return an error if part of the parent destination path does not exist', function(done) {
-  //   var fs = util.fs().promises;
-
-  //   fs.symlink('/', '/tmp/mydir', function(error) {
-  //     expect(error).to.exist;
-  //     expect(error.code).to.equal('ENOENT');
-  //     done();
-  //   });
-  // });
-
-  // it('should return an error if the destination path already exists', function(done) {
-  //   var fs = util.fs().promises;
-
-  //   fs.symlink('/tmp', '/', function(error) {
-  //     expect(error).to.exist;
-  //     expect(error.code).to.equal('EEXIST');
-  //     done();
-  //   });
-  // });
-
-  // it('should create a symlink', function(done) {
-  //   var fs = util.fs().promises;
-
-  //   fs.symlink('/', '/myfile', function(error) {
-  //     expect(error).not.to.exist;
-
-  //     fs.stat('/myfile', function(err, stats) {
-  //       expect(error).not.to.exist;
-  //       expect(stats.type).to.equal('DIRECTORY');
-  //       done();
-  //     });
-  //   });
-  // });
 });
