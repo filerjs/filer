9efafc248dbbf1f7e8ee0a7e866c5de7c6259996
Nick Skuybeda <mskuybeda@myseneca.ca>
2018-09-28 09:46:36 -0400

    fixing whitespaces

--- tests/spec/fs.mknod.spec.js
+++ tests/spec/fs.mknod.spec.js
@@ -1,64 +1,64 @@
 var util = require('../lib/test-utils.js');
 var expect = require('chai').expect;
 
-describe('fs.mknod', function () {
+describe('fs.mknod', function(){
   beforeEach(util.setup);
   afterEach(util.cleanup);
 
-  it('should be a function', function (done) {
+  it('should be a function', function(done){
     var fs = util.fs();
     expect(fs.mknod).to.be.a('function');
     done();
   });
 
-  it('should return an error if part of the parent path does not exist', function (done) {
+  it('should return an error if part of the parent path does not exist', function(done){
     var fs = util.fs();
 
-    fs.mknod('/dir/mydir', 'DIRECTORY', function (error) {
+    fs.mknod('/dir/mydir', 'DIRECTORY', function(error){
       expect(error.code).to.equal('ENOENT');
       done();
     });
   });
 
-  it('should return an error if path already exists', function (done) {
+  it('should return an error if path already exists', function(done){
     var fs = util.fs();
 
-    fs.mknod('/', 'DIRECTORY', function (error) {
+    fs.mknod('/', 'DIRECTORY', function(error){
       expect(error.code).to.equal('EEXIST');
       done();
     });
   });
 
-  it('should return an error if the parent node is not a directory', function (done) {
+  it('should return an error if the parent node is not a directory', function(done){
     var fs = util.fs();
 
-    fs.mknod('/file', 'FILE', function (error) {
+    fs.mknod('/file', 'FILE', function(error){
       if (error) throw error;
 
-      fs.mknod('/file/myfile', 'FILE', function (error) {
+      fs.mknod('/file/myfile', 'FILE', function(error){
         expect(error.code).to.equal('ENOTDIR');
         done();
       });
     });
   });
 
-  it('should return an error if the mode provided is not DIRECTORY or FILE', function (done) {
+  it('should return an error if the mode provided is not DIRECTORY or FILE', function(done){
     var fs = util.fs();
 
-    fs.mknod('/symlink', 'SYMLINK', function (error) {
+    fs.mknod('/symlink', 'SYMLINK', function(error){
       expect(error).to.exist;
       expect(error.code).to.equal('EINVAL');
       done();
     });
   });
 
-  it('should make a new directory', function (done) {
+  it('should make a new directory', function(done){
     var fs = util.fs();
 
-    fs.mknod('/dir', 'DIRECTORY', function (error) {
+    fs.mknod('/dir', 'DIRECTORY', function(error){
       if (error) throw error;
 
-      fs.stat('/dir', function (error, stats) {
+      fs.stat('/dir', function(error, stats){
         expect(error).not.to.exist;
         expect(stats.type).to.equal('DIRECTORY');
         done();
@@ -66,24 +66,23 @@ describe('fs.mknod', function () {
     });
   });
 
-  it('should make a new file', function (done) {
+  it('should make a new file', function(done){
     var fs = util.fs();
 
-    fs.mknod('/file', 'FILE', function (error) {
+    fs.mknod('/file', 'FILE', function(error){
       if (error) throw error;
 
-      fs.stat('/file', function (error, result) {
+      fs.stat('/file', function(error, result){
         expect(error).not.to.exist;
         expect(result.type).to.equal('FILE');
         done();
       });
     });
   });
-  describe.only('Promise test mknod', function () {
+  describe('Promise test mknod', function(){
 
-    it('should return an error if part of the parent path does not exist', function () {
+    it('should return an error if part of the parent path does not exist', function(){
       var fs = util.fs().promises;
-
 
       return fs.mknod('/dir/mydir', 'DIRECTORY')
         .catch(error => {
