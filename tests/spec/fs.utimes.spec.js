1e9234efa6253276cb34bc82bfb77427b9b6095d
Chaya Danzinger <chayadanz@gmail.com>
2018-09-28 15:03:54 -0400

    first two promise tests initial commit

--- tests/spec/fs.utimes.spec.js
+++ tests/spec/fs.utimes.spec.js
@@ -1,19 +1,19 @@
 var util = require('../lib/test-utils.js');
 var expect = require('chai').expect;
 
-describe('fs.utimes', function() {
+describe('fs.utimes', function () {
   beforeEach(util.setup);
   afterEach(util.cleanup);
 
-  it('should be a function', function() {
+  it('should be a function', function () {
     var fs = util.fs();
     expect(fs.utimes).to.be.a('function');
   });
 
-  it('should error when atime is negative', function(done) {
+  it('should error when atime is negative', function (done) {
     var fs = util.fs();
 
-    fs.writeFile('/testfile', '', function(error) {
+    fs.writeFile('/testfile', '', function (error) {
       if (error) throw error;
 
       fs.utimes('/testfile', -1, Date.now(), function (error) {
@@ -24,10 +24,10 @@ describe('fs.utimes', function() {
     });
   });
 
-  it('should error when mtime is negative', function(done) {
+  it('should error when mtime is negative', function (done) {
     var fs = util.fs();
 
-    fs.writeFile('/testfile', '', function(error) {
+    fs.writeFile('/testfile', '', function (error) {
       if (error) throw error;
 
       fs.utimes('/testfile', Date.now(), -1, function (error) {
@@ -38,7 +38,7 @@ describe('fs.utimes', function() {
     });
   });
 
-  it('should error when atime is as invalid number', function(done) {
+  it('should error when atime is as invalid number', function (done) {
     var fs = util.fs();
 
     fs.writeFile('/testfile', '', function (error) {
@@ -52,7 +52,7 @@ describe('fs.utimes', function() {
     });
   });
 
-  it('should error when path does not exist', function(done) {
+  it('should error when path does not exist', function (done) {
     var fs = util.fs();
     var atime = Date.parse('1 Oct 2000 15:33:22');
     var mtime = Date.parse('30 Sep 2000 06:43:54');
@@ -64,7 +64,7 @@ describe('fs.utimes', function() {
     });
   });
 
-  it('should error when mtime is an invalid number', function(done) {
+  it('should error when mtime is an invalid number', function (done) {
     var fs = util.fs();
 
     fs.writeFile('/testfile', '', function (error) {
@@ -78,7 +78,7 @@ describe('fs.utimes', function() {
     });
   });
 
-  it('should error when file descriptor is invalid', function(done) {
+  it('should error when file descriptor is invalid', function (done) {
     var fs = util.fs();
     var atime = Date.parse('1 Oct 2000 15:33:22');
     var mtime = Date.parse('30 Sep 2000 06:43:54');
@@ -90,7 +90,7 @@ describe('fs.utimes', function() {
     });
   });
 
-  it('should change atime and mtime of a file path', function(done) {
+  it('should change atime and mtime of a file path', function (done) {
     var fs = util.fs();
     var atime = Date.parse('1 Oct 2000 15:33:22');
     var mtime = Date.parse('30 Sep 2000 06:43:54');
@@ -110,7 +110,7 @@ describe('fs.utimes', function() {
     });
   });
 
-  it ('should change atime and mtime for a valid file descriptor', function(done) {
+  it('should change atime and mtime for a valid file descriptor', function (done) {
     var fs = util.fs();
     var ofd;
     var atime = Date.parse('1 Oct 2000 15:33:22');
@@ -132,7 +132,7 @@ describe('fs.utimes', function() {
     });
   });
 
-  it('should update atime and mtime of directory path', function(done) {
+  it('should update atime and mtime of directory path', function (done) {
     var fs = util.fs();
     var atime = Date.parse('1 Oct 2000 15:33:22');
     var mtime = Date.parse('30 Sep 2000 06:43:54');
@@ -152,7 +152,7 @@ describe('fs.utimes', function() {
     });
   });
 
-  it('should update atime and mtime using current time if arguments are null', function(done) {
+  it('should update atime and mtime using current time if arguments are null', function (done) {
     var fs = util.fs();
 
     fs.writeFile('/myfile', '', function (error) {
@@ -171,6 +171,122 @@ describe('fs.utimes', function() {
           expect(then - stat.mtime).to.be.at.most(delta);
           done();
         });
+      });
+    });
+  });
+});
+
+describe('fs.promises.utimes', function () {
+  beforeEach(util.setup);
+  afterEach(util.cleanup);
+
+
+  it('should be a function', function () {
+    var fs = util.fs().promises;
+    expect(fs.utimes).to.be.a('function');
+  });
+
+  it('should error when atime is negative', function() {
+    var fs = util.fs().promises;
+    return fs.writeFile('/testfile', '')
+      .then(function() {
+        fs.utimes('/testfile', -1, Date.now())
+        .then(() => {
+        }, function(err) {
+          expect(error).to.exist;
+          expect(error.code).to.equal('EINVAL');
+        })
+      })
+      .catch(function(err) {
+        throw err; 
+      });
+  });
+
+  it('should create a link to an existing file', function (done) {
+    var fs = util.fs();
+    fs.open('/myfile', 'w+', function (error, fd) {
+      if (error) throw error;
+      fs.close(fd, function (error) {
+        if (error) throw error;
+        fs.promises.link('/myfile', '/myotherfile').then(function () {
+          fs.stat('/myfile', function (error, result) {
+            if (error) throw error;
+
+            var _oldstats = result;
+            fs.stat('/myotherfile', function (error, result) {
+              expect(error).not.to.exist;
+              expect(result.nlinks).to.equal(2);
+              expect(result.dev).to.equal(_oldstats.dev);
+              expect(result.node).to.equal(_oldstats.node);
+              expect(result.size).to.equal(_oldstats.size);
+              expect(result.type).to.equal(_oldstats.type);
+              done();
+            });
+          },
+            function (error) { throw error; }
+          );
+        });
+      });
+    });
+  });
+
+  it('should create hard link to identical data node', function (done) {
+    var fs = util.fs();
+    var contents = 'Hello World!';
+    fs.writeFile('/file', contents, function (err) {
+      if (err) throw err;
+      fs.promises.link('/file', '/hlink', function (err) {
+        if (err) throw err;
+        fs.readFile('/file', 'utf8', function (err, fileData) {
+          if (err) throw err;
+          fs.readFile('/hlink', 'utf8', function (err, hlinkData) {
+            if (err) throw err;
+            expect(fileData).to.equal(hlinkData);
+            done();
+          });
+        });
+      });
+    });
+  });
+
+  it('should not follow symbolic links', function (done) {
+    var fs = util.fs();
+    fs.stat('/', function (error, result) {
+      if (error) throw error;
+      expect(result).to.exist;
+      fs.symlink('/', '/myfileLink', function (error) {
+        if (error) throw error;
+        fs.promises.link('/myfileLink', '/myotherfile').then(function () {
+          if (error) throw error;
+
+          fs.lstat('/myfileLink', function (error, result) {
+
+            var _linkstats = result;
+            fs.lstat('/myotherfile', function (error, result) {
+              expect(error).not.to.exist;
+              expect(result.dev).to.equal(_linkstats.dev);
+              expect(result.node).to.equal(_linkstats.node);
+              expect(result.size).to.equal(_linkstats.size);
+              expect(result.type).to.equal(_linkstats.type);
+              expect(result.nlinks).to.equal(2);
+              done();
+            });
+          },
+            function (error) { throw error; }
+          );
+        });
+      });
+    });
+  });
+  
+  it('should not allow links to a directory', function (done) {
+    var fs = util.fs();
+    fs.mkdir('/mydir', function (error) {
+      if (error) throw error;
+      fs.promises.link('/mydir', '/mydirlink', function (error) {
+        expect(error).to.exist;
+        expect(error.code).to.equal('EPERM');
+        done();
       });
     });
   });
