399478c355d2e6a5db0534304dc3065164c047fb
rscotchmer <raescotchmer@gmail.com>
2019-01-29 10:36:27 -0500

    refactored code in tests/spec/filer.buffer.spec.js

--- tests/spec/filer.buffer.spec.js
+++ tests/spec/filer.buffer.spec.js
@@ -1,5 +1,7 @@
-var Filer = require('../../src');
-var expect = require('chai').expect;
+'use strict';
+
+const Filer = require('../../src');
+const expect = require('chai').expect;
 
 describe('Filer.Buffer', function() {
 
@@ -12,37 +14,37 @@ describe('Filer.Buffer', function() {
   });
 
   it('should support .isBuffer()', function() {
-    var buf = Buffer.alloc(0);
+    let buf = Buffer.alloc(0);
     expect(Buffer.isBuffer(buf)).to.be.true;
   });
 
   describe('Deprecation checks - constructor vs. class method init', function() {
 
     it('should allow new Buffer(array)', function() {
-      var arr = [1, 2, 3];
-      var buf1 = new Buffer(arr);
-      var buf2 = new Buffer.from(arr);
+      let arr = [1, 2, 3];
+      let buf1 = new Buffer(arr);
+      let buf2 = new Buffer.from(arr);
       expect(buf1).to.deep.equal(buf2);
     });
 
     it('should allow new Buffer(ArrayBuffer)', function() {
-      var arrayBuffer = (new Uint8Array([1, 2, 3])).buffer;
-      var buf1 = new Buffer(arrayBuffer);
-      var buf2 = Buffer.from(arrayBuffer);
+      let arrayBuffer = (new Uint8Array([1, 2, 3])).buffer;
+      let buf1 = new Buffer(arrayBuffer);
+      let buf2 = Buffer.from(arrayBuffer);
       expect(buf1).to.deep.equal(buf2);
     });
 
     it('should allow new Buffer(ArrayBuffer)', function() {
-      var buffer = new Buffer.from([1, 2, 3]);
-      var buf1 = new Buffer(buffer);
-      var buf2 = Buffer.from(buffer);
+      let buffer = new Buffer.from([1, 2, 3]);
+      let buf1 = new Buffer(buffer);
+      let buf2 = Buffer.from(buffer);
       expect(buf1).to.deep.equal(buf2);
     });
 
     it('should allow new Buffer(string)', function() {
-      var s = 'Hello World';
-      var buf1 = new Buffer(s);
-      var buf2 = Buffer.from(s);
+      let s = 'Hello World';
+      let buf1 = new Buffer(s);
+      let buf2 = Buffer.from(s);
       expect(buf1).to.deep.equal(buf2);
     });
   });
