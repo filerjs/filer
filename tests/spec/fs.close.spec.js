2f58a2445737e8642f9c082fdf1b962c15183281
Olena Vyshnevska <ovyshnevska@myseneca.ca>
2019-01-29 14:56:38 -0500

    code refactoring, added strict mode

--- tests/spec/fs.close.spec.js
+++ tests/spec/fs.close.spec.js
@@ -1,23 +1,25 @@
-var util = require('../lib/test-utils.js');
-var expect = require('chai').expect;
+'use strict'; 
+
+const util = require('../lib/test-utils.js');
+const expect = require('chai').expect;
 
 describe('fs.close', function() {
   beforeEach(util.setup);
   afterEach(util.cleanup);
 
   it('should be a function', function() {
-    var fs = util.fs();
+    const fs = util.fs();
     expect(typeof fs.close).to.equal('function');
   });
 
   it('should release the file descriptor', function(done) {
-    var buffer = Buffer.alloc(0);
-    var fs = util.fs();
+    const buffer = Buffer.alloc(0);
+    const fs = util.fs();
 
     fs.open('/myfile', 'w+', function(error, result) {
       if(error) throw error;
 
-      var fd = result;
+      const fd = result;
       fs.close(fd, function(error) {
         if(error) throw error;
 
