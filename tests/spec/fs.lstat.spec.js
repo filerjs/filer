e0cdab445b810bd99ade3934544b4fab4169515b
hoaianhkhang <46699312+hoaianhkhang@users.noreply.github.com>
2019-01-30 14:15:59 -0500

    Update fs.lstat.spec.js

--- tests/spec/fs.lstat.spec.js
+++ tests/spec/fs.lstat.spec.js
@@ -1,74 +1,50 @@
-var util = require('../lib/test-utils.js');
-var expect = require('chai').expect;
+"use strict";
+const util = require('../lib/test-utils.js');
+const expect = require('chai').expect;
 
-describe('fs.lstat', function() {
-  beforeEach(util.setup);
-  afterEach(util.cleanup);
+describe('fs.lstat', function () {
+    beforeEach(util.setup);
+    afterEach(util.cleanup);
 
-  it('should be a function', function() {
-    var fs = util.fs();
-    expect(typeof fs.lstat).to.equal('function');
-  });
-
-  it('should return an error if path does not exist', function(done) {
-    var fs = util.fs();
-
-    fs.lstat('/tmp', function(error, result) {
-      expect(error).to.exist;
-      expect(error.code).to.equal('ENOENT');
-      expect(result).not.to.exist;
-      done();
+    it('should be a function', function () {
+        const fs = util.fs();
+        expect(typeof fs.lstat).to.equal('function');
     });
-  });
 
-  it('should return a stat object if path is not a symbolic link', function(done) {
-    var fs = util.fs();
+    it('should return an error if path does not exist', function (done) {
+        let fs = util.fs();
 
-    fs.lstat('/', function(error, result) {
-      expect(error).not.to.exist;
-      expect(result).to.exist;
-      expect(result.isDirectory()).to.be.true;
-      done();
+        fs.lstat('/tmp', function (error, result) {
+            expect(error).to.exist;
+            expect(error.code).to.equal('ENOENT');
+            expect(result).not.to.exist;
+            done();
+        });
     });
-  });
 
-  it('should return a stat object if path is a symbolic link', function(done) {
-    var fs = util.fs();
+    it('should return a stat object if path is not a symbolic link', function (done) {
+        let fs = util.fs();
 
-    fs.symlink('/', '/mylink', function(error) {
-      if(error) throw error;
-
-      fs.lstat('/mylink', function(error, result) {
-        expect(error).not.to.exist;
-        expect(result).to.exist;
-        expect(result.isSymbolicLink()).to.be.true;
-        done();
-      });
+        fs.lstat('/', function (error, result) {
+            expect(error).not.to.exist;
+            expect(result).to.exist;
+            expect(result.type).to.equal('DIRECTORY');
+            done();
+        });
     });
-  });
-});
 
-describe('fs.promises.lstat', () => {
-  beforeEach(util.setup);
-  afterEach(util.cleanup);
+    it('should return a stat object if path is a symbolic link', function (done) {
+        let fs = util.fs();
 
-  it('should return an error if path does not exist', () => {
-    var fsPromises = util.fs().promises;
-    
-    return fsPromises.lstat('/tmp')
-      .catch( error => {
-        expect(error).to.exist;
-        expect(error.code).to.equal('ENOENT');
-      });
-  });
+        fs.symlink('/', '/mylink', function (error) {
+            if (error) throw error;
 
-  it('should return a stat object if path is not a symbolic link', () => {
-    var fsPromises = util.fs().promises;
-
-    return fsPromises.lstat('/')
-      .then(result => {
-        expect(result).to.exist;
-        expect(result.isDirectory()).to.be.true;
-      });
-  });
+            fs.lstat('/mylink', function (error, result) {
+                expect(error).not.to.exist;
+                expect(result).to.exist;
+                expect(result.type).to.equal('SYMLINK');
+                done();
+            });
+        });
+    });
 });
