e3e9b4d228341b4cd965e8bf6a964ee8eb1fe28d
FineHH <finehhtesting@gmail.com>
2019-01-30 18:46:18 -0500

    Fixes for review comments

--- tests/spec/fs.shell.spec.js
+++ tests/spec/fs.shell.spec.js
@@ -1,26 +1,28 @@
-var Filer = require('../../src');
-var util = require('../lib/test-utils.js');
-var expect = require('chai').expect;
+'use strict';
+
+const Filer = require('../../src');
+const util = require('../lib/test-utils.js');
+const expect = require('chai').expect;
 
 describe('fs.Shell', function() {
   beforeEach(util.setup);
   afterEach(util.cleanup);
 
   it('is a function', function() {
-    var fs = util.fs();
+    const fs = util.fs();
     expect(typeof fs.Shell).to.equal('function');
   });
 
   it('should return a FileSystemShell instance', function() {
-    var fs = util.fs();
-    var sh = new fs.Shell();
+    const fs = util.fs();
+    const sh = new fs.Shell();
 
     expect(sh.prototype).to.deep.equal((new Filer.Shell(fs)).prototype);
   });
 
   it('should reflect changes to the prototype', function(){
-    var fs = util.fs();
-    var sh = new fs.Shell();
+    const fs = util.fs();
+    const sh = new fs.Shell();
 
     Filer.Shell.prototype.test = 'foo';
 
