43b72e1ab2c8e499640ae5eac3f70d5fd656d6e7
rscotchmer <raescotchmer@gmail.com>
2019-02-01 10:32:30 -0500

    roll back changes to package-lock.json, change lets to consts in filer.buffer.spec.js, added new line at the end of filer.buffer.spec.js

--- tests/spec/filer.buffer.spec.js
+++ tests/spec/filer.buffer.spec.js
@@ -14,37 +14,37 @@ describe('Filer.Buffer', function() {
   });
 
   it('should support .isBuffer()', function() {
-    let buf = Buffer.alloc(0);
+    const buf = Buffer.alloc(0);
     expect(Buffer.isBuffer(buf)).to.be.true;
   });
 
   describe('Deprecation checks - constructor vs. class method init', function() {
 
     it('should allow new Buffer(array)', function() {
-      let arr = [1, 2, 3];
-      let buf1 = new Buffer(arr);
-      let buf2 = new Buffer.from(arr);
+      const arr = [1, 2, 3];
+      const buf1 = new Buffer(arr);
+      const buf2 = new Buffer.from(arr);
       expect(buf1).to.deep.equal(buf2);
     });
 
     it('should allow new Buffer(ArrayBuffer)', function() {
-      let arrayBuffer = (new Uint8Array([1, 2, 3])).buffer;
-      let buf1 = new Buffer(arrayBuffer);
-      let buf2 = Buffer.from(arrayBuffer);
+      const arrayBuffer = (new Uint8Array([1, 2, 3])).buffer;
+      const buf1 = new Buffer(arrayBuffer);
+      const buf2 = Buffer.from(arrayBuffer);
       expect(buf1).to.deep.equal(buf2);
     });
 
     it('should allow new Buffer(ArrayBuffer)', function() {
-      let buffer = new Buffer.from([1, 2, 3]);
-      let buf1 = new Buffer(buffer);
-      let buf2 = Buffer.from(buffer);
+      const buffer = new Buffer.from([1, 2, 3]);
+      const buf1 = new Buffer(buffer);
+      const buf2 = Buffer.from(buffer);
       expect(buf1).to.deep.equal(buf2);
     });
 
     it('should allow new Buffer(string)', function() {
-      let s = 'Hello World';
-      let buf1 = new Buffer(s);
-      let buf2 = Buffer.from(s);
+      const s = 'Hello World';
+      const buf1 = new Buffer(s);
+      const buf2 = Buffer.from(s);
       expect(buf1).to.deep.equal(buf2);
     });
   });
