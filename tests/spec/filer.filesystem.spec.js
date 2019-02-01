8eaaeeed17192b317c847da3fa7040c82d3e25fc
Woosle Park <woosle57@hotmail.com>
2019-01-31 20:23:57 -0500

    Fix issue #681: Updated filer/tests/spec/filer.filesystem.spec.js (#695)
    * issue 681 fix
    
    * issue 681 revision
    
    * issue 681 revision v2

--- tests/spec/filer.filesystem.spec.js
+++ tests/spec/filer.filesystem.spec.js
@@ -1,16 +1,17 @@
-var Filer = require('../../src');
-var util = require('../lib/test-utils.js');
-var expect = require('chai').expect;
+'use strict';
+const Filer = require('../../src');
+const util = require('../lib/test-utils.js');
+const expect = require('chai').expect;
 
 describe('Filer.FileSystem', function() {
   beforeEach(util.setup);
   afterEach(util.cleanup);
 
   it('should properly mount new or existing filesystem', function(done) {
-    var provider = util.provider().provider;
+    const provider = util.provider().provider;
 
     // 1) Should be able to open a new filesystem, and get empty root
-    var fs1 = new Filer.FileSystem({provider: provider}, function() {
+    const fs1 = new Filer.FileSystem({provider: provider}, function() {
       fs1.readdir('/', function(err, entries) {
         expect(err).not.to.exist;
         expect(entries).to.be.an('array');
@@ -20,7 +21,7 @@ describe('Filer.FileSystem', function() {
           if(err) throw err;
 
           // 2) Should be able to open an existing filesystem
-          var fs2 = new Filer.FileSystem({provider: provider}, function() {
+          const fs2 = new Filer.FileSystem({provider: provider}, function() {
             fs2.readdir('/', function(err, entries) {
               expect(err).not.to.exist;
               expect(entries).to.be.an('array');
@@ -29,7 +30,7 @@ describe('Filer.FileSystem', function() {
 
 
               // 3) FORMAT flag should wipe an existing filesystem
-              var fs3 = new Filer.FileSystem({provider: provider, flags: ['FORMAT']}, function() {
+              const fs3 = new Filer.FileSystem({provider: provider, flags: ['FORMAT']}, function() {
                 fs3.readdir('/', function(err, entries) {
                   expect(err).not.to.exist;
                   expect(entries).to.be.an('array');
