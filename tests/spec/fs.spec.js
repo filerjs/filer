f2201e7a74006491b9651d9e3de45f5809c56385
Paul Moon <bakamono.moon@gmail.com>
2019-02-12 14:46:56 -0500

    Update fs.spec.js to use strict mode and changed var to let (#693)
    * Update fs.spec.js
    
    * Update fs.spec.js
    
    Changed global variables
    
    * Update fs.spec.js
    
    changed let to const

--- tests/spec/fs.spec.js
+++ tests/spec/fs.spec.js
@@ -1,19 +1,21 @@
-var Filer = require('../../src');
-var util = require('../lib/test-utils.js');
-var expect = require('chai').expect;
+'use strict';
+
+const Filer = require('../../src');
+const util = require('../lib/test-utils.js');
+const expect = require('chai').expect;
 
 describe('fs', function() {
   beforeEach(util.setup);
   afterEach(util.cleanup);
 
   it('is an object', function() {
-    var fs = util.fs();
+    const fs = util.fs();
     expect(typeof fs).to.equal('object');
     expect(fs).to.be.an.instanceof(Filer.FileSystem);
   });
 
   it('should have a root directory', function(done) {
-    var fs = util.fs();
+    const fs = util.fs();
     fs.stat('/', function(error, result) {
       expect(error).not.to.exist;
       expect(result).to.exist;
