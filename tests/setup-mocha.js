57dc4e9481f33b35dcc792108f1712317a78c2ec
Ben Heidemann <ben@heidemann.co.uk>
2021-03-21 13:51:53 +0000

    chore: fix linting issues

--- tests/setup-mocha.js
+++ tests/setup-mocha.js
@@ -3,6 +3,6 @@ var mocha = require('mocha');
 mocha.setup('bdd').timeout(10000).slow(250);
 
 window.onload = function() {
-    mocha.checkLeaks();
-    mocha.run();
+  mocha.checkLeaks();
+  mocha.run();
 };
