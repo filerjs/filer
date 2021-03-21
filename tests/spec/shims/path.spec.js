3e88aeca92b2bbb4637fcd73cf1e753a6ec39a85
Ben Heidemann <56122437+bcheidemann@users.noreply.github.com>
2021-03-21 17:57:34 +0000

    Fix issue #766: Remove describe.only from shim tests (#767)
    * test: replace describe.only in shim tests with describes so all tests run
    
    * test: move require chai to top of file for consistency with other tests
    
    * chore: npm install
    
    * test: replace describe.only in shim tests with describes so all tests run
    
    * test: move require chai to top of file for consistency with other tests
    
    * chore: npm install
    
    * Revert "chore: npm install"
    
    This reverts commit cddeef421bc51e3549faf7dc97c2817504200472.
    
    * Revert "test: move require chai to top of file for consistency with other tests"
    
    This reverts commit 40df7912fb0de31f796cc8e58afd40da87808929.

--- tests/spec/shims/path.spec.js
+++ tests/spec/shims/path.spec.js
@@ -3,7 +3,7 @@ const expect = require('chai').expect;
 const path = require('../../../shims/path').default;
 const pathActual = require('../../../src/index').path;
 
-describe.only('path shim', () => {
+describe('path shim', () => {
   it('should be defined', () => {
     expect(path).to.not.be.undefined;
   });
