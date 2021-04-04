3b9fafc53ef8c881a61c3e567aa047fd58a0d8c8
Ben Heidemann <ben@heidemann.co.uk>
2021-04-04 02:17:01 +0100

    style: fix linting issues in webpack plugin

--- src/webpack-plugin/processors.js
+++ src/webpack-plugin/processors.js
@@ -2,32 +2,32 @@ const ROOT_DIR_TAG = '<rootDir>';
 const CWD = process.cwd();
 
 module.exports = {
-    filerDir: {
-        process: function(value) {
-            if (!value) {
-                return path.join(CWD, 'node_modules', 'filer');
-            }
-            return value.replace(ROOT_DIR_TAG, CWD);
-        },
+  filerDir: {
+    process: function(value) {
+      if (!value) {
+        return path.join(CWD, 'node_modules', 'filer');
+      }
+      return value.replace(ROOT_DIR_TAG, CWD);
     },
-    shimsDir: {
-        process: function(value) {
-            if (!value) {
-                return path.join(CWD, 'node_modules', 'filer', 'shims');
-            }
-            return value.replace(ROOT_DIR_TAG, CWD);
-        }
+  },
+  shimsDir: {
+    process: function(value) {
+      if (!value) {
+        return path.join(CWD, 'node_modules', 'filer', 'shims');
+      }
+      return value.replace(ROOT_DIR_TAG, CWD);
+    }
+  },
+  shimFs: { default: true },
+  shimPath: { default: true},
+  shimBuffer: { default: true},
+  fsProvider: { default: 'default'},
+  fsProviderDir: {
+    process: function(value) {
+      if (!value) {
+        return path.join(CWD, 'node_modules', 'filer', 'shims', 'providers');
+      }
+      return value.replace(ROOT_DIR_TAG, CWD);
     },
-    shimFs: { default: true },
-    shimPath: { default: true},
-    shimBuffer: { default: true},
-    fsProvider: { default: 'default'},
-    fsProviderDir: {
-        process: function(value) {
-            if (!value) {
-                return path.join(CWD, 'node_modules', 'filer', 'shims', 'providers');
-            }
-            return value.replace(ROOT_DIR_TAG, CWD);
-        },
-    },
+  },
 };
--- src/webpack-plugin/schema.js
+++ src/webpack-plugin/schema.js
@@ -1,26 +1,26 @@
 module.exports = {
-    type: 'object',
-    properties: {
-        filerDir: {
-            type: 'string',
-        },
-        shimsDir: {
-            type: 'string',
-        },
-        shimFs: {
-            type: 'boolean',
-        },
-        shimPath: {
-            type: 'boolean',
-        },
-        shimBuffer: {
-            type: 'boolean',
-        },
-        fsProvider: {
-            type: 'string',
-        },
-        fsProviderDir: {
-            type: 'string',
-        },
-    }
+  type: 'object',
+  properties: {
+    filerDir: {
+      type: 'string',
+    },
+    shimsDir: {
+      type: 'string',
+    },
+    shimFs: {
+      type: 'boolean',
+    },
+    shimPath: {
+      type: 'boolean',
+    },
+    shimBuffer: {
+      type: 'boolean',
+    },
+    fsProvider: {
+      type: 'string',
+    },
+    fsProviderDir: {
+      type: 'string',
+    },
+  }
 };
